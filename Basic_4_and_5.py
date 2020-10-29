c761bfcd52d6313f73683e9a4d8eaeeba9198f4b
quietman2 <quietkulak@gmail.com>
2020-10-29 23:38:55 +0200

    Added the 'Basic_3' and 'Basic_4_and_5' files

---
+++ Basic_4_and_5.py
@@ -0,0 +1,40 @@
+
+
+'''Below is the 4nd task from the Basic'''
+
+
+
+"use lists, dictionaries and tuples"
+
+# list
+basket_of_fruits = ['apple', 'cherry', 'banana', 'orange', 'plum', 'pear', 'strawberry']
+for fruit in basket_of_fruits:
+    print(fruit)
+
+
+# tuple
+tuple_made_of_list = tuple(basket_of_fruits)
+print('the fruit under the index 3 is ' + str(tuple_made_of_list[3]))
+
+
+
+# dict
+basket_of_apples = {
+    "red apple": 5,
+    "green apple": 7,
+    "yellow apple": 12,
+    "strange apple": 4,
+}
+
+print("All the apples gathered today " + str(basket_of_apples))
+
+
+
+"Use  get, add, remove elements for list structures"
+
+basket_of_fruits.remove('cherry')
+print('the cherry did not survive ' + str(basket_of_fruits))
+
+
+basket_of_fruits.insert(7, 'avocado')
+print(basket_of_fruits)
