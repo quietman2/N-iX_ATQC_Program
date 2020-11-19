901ed8934059ffc4bf45f9bc39fcfe3386310dcd
quietman2 <quietkulak@gmail.com>
2020-11-19 22:52:48 +0200

    Added a 'Basic_3 file containing the 2nd task from the Basic block

--- Basic_6.py
+++ Basic_6.py
@@ -1,4 +1,5 @@
-
+import pickle
+import json
 
 "Below is the 6th task from the Basic"
 from typing import List, Any
@@ -34,3 +35,80 @@ print(digits[2] + digits[3])
 print(digits[1] / digits[2])
 
 
+
+"Create class and serialize it; print result;" \
+" deserialize it; print result; // for JS serialize to JSON"
+
+class Trainee:
+    def __init__(self, name, age, project, is_on_probation):
+        self.name = name
+        self.age = age
+        self.project = project
+        self.is_on_probation = is_on_probation
+
+trainee_1 = Trainee('Volodymyr', 30, 'Cardo', False)
+
+pickled_trainee = pickle.dumps(trainee_1, 3)
+print(pickled_trainee)
+
+unpickled_trainee = pickle.loads(pickled_trainee)
+print(unpickled_trainee)
+
+
+
+"Create string that contains JSON { \"name\":\"John\", \"age\":30, \"car\":null }; " \
+"parse this string into JSON object and print it's name and age."
+
+str_json = '{ "name":"John", "age":30, "car":null }'
+data_json = json.loads(str_json)
+print(data_json['name'], data_json['age'])
+
+
+
+"STRINGS:"
+
+# Create 2 different strings. Concatenate them and print result. Explain what types of concatenation exists.
+a = "Python is "
+b = "awesome!"
+print(a + b)
+print(a + "awesome!")
+
+
+# · Create string "   egweerw  ererferw  kuy  yu i ". Print its length;  Trim string and print result. Print length of trimmed string.
+strange_string = "   egweerw  ererferw  kuy  yu i "
+print("lenght of the original string is " + str(len(strange_string)))
+print("this string is trimmed: " + strange_string.strip())
+print("lenght of the trimmed string is " + str(len(strange_string.strip())))
+
+
+# · Create string "   egweerw  ererferw  kuy  yu i ". Split string by spaces and print each value in a separate row.
+for i in strange_string.split(" "):
+    print(i)
+
+
+# · Create string "   egweerw  ererferw  kuy  yu i ". Split string by spaces; Clean empty spaces from result; Print each result value in a separate row.
+for i in strange_string.strip().split():
+    print(i)
+
+
+# · Create string "   egweerw  ererferw  kuy  yu i ". Convert all characters to uppercase. Print result;
+print(strange_string.upper())
+
+
+# · Create string "   egweerw  ererferw  kuy  yu i ". Convert all 'w' to uppercase. Print result;
+print(strange_string.replace("w", "W"))
+
+
+# · Create string "   egweerw  ererferw  kuy  yu i ". Convert first 'w' to uppercase. Print result;
+print(strange_string.replace("w", "W", 1))
+
+# · Create string "this item previous price $5.99, Sale price $1.99" - parse original and sale price from string and print them.
+strange_string2 = "this item previous price $5.99, Sale price $1.99"
+
+original_price = ''
+sale_price = ''
+for i in strange_string2:
+    if i.isdigit():
+        original_price += i
+        sale_price += i
+print(sale_price, original_price)
